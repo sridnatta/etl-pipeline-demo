da28ec5e27e769fb020ece4480612fc23a80ef26
Sri Natta <sri.divya419@gmail.com>
2026-08-07 22:08:48 -0400

    added comment to extract

--- etl/extract.py
+++ etl/extract.py
@@ -6,5 +6,5 @@ def extract_data(path="data/imput.csv"):
         reader = csv.DictReader(f)
         for row in reader:
             rows.append(row)
-    ## test feature extract
+    ## test feature extract 1 - testing rebase
     return rows
