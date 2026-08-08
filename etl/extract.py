df692614b0bc9cb068eba6ec650b4b823b4bcb65
Sri Natta <sri.divya419@gmail.com>
2026-08-07 22:11:51 -0400

    added second change to feature branch

--- etl/extract.py
+++ etl/extract.py
@@ -6,5 +6,7 @@ def extract_data(path="data/imput.csv"):
         reader = csv.DictReader(f)
         for row in reader:
             rows.append(row)
+
     ## test feature extract 1 - testing rebase
+    # adding additional changes to extract
     return rows
